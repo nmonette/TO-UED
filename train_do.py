4fe055343cf188c9856274bc68c11137b2a32a37
Nathan Monette <nathanmonette1@gmail.com>
2024-04-30 19:11:50 -0700

    got rid of make_train_old

--- train_do.py
+++ train_do.py
@@ -11,78 +11,6 @@ from experiments.parse_args import parse_args
 from experiments.logging import init_logger, log_results
 from meta.meta import make_lpg_train_step, create_lpg_train_state
 
-def make_train_old(args): 
-    def _train_fn(rng):
-        """
-        Sample a random environment for the evaluator and the meta-policy, 
-        then add those to each buffer, then initialize the nash strategy
-        of each as [1]. Then, scan over _meta_train_loop.  
-        """      
-        # --- Initialize nash distributions ---
-        train_nash = jnp.zeros((args.buffer_size, )).at[0].set(1) 
-        eval_nash = jnp.zeros((args.buffer_size, )).at[0].set(1) 
-
-        # --- Initialize sampler and buffers ---
-        level_sampler = NashSampler(args)
-        rng, _rng = jax.random.split(rng)
-        train_buffer, eval_buffer = level_sampler.initialize_buffers(_rng)
-
-        # --- Double Oracle (environment generating) Loop ---
-        def _meta_do_loop(carry, t):
-            rng, train_buffer, eval_buffer, train_nash, eval_nash = carry
-
-            # --- Get best response levels ---
-            rng, train_rng, eval_rng, nash_rng = jax.random.split(rng, 4)
-            new_train = level_sampler.get_train_br(train_rng, eval_nash, eval_buffer)
-            new_eval = level_sampler.get_eval_br(eval_rng, train_nash, train_buffer)
-
-            train_buffer = train_buffer.replace(level=train_buffer.level.at[t].set(new_train))
-            eval_buffer = eval_buffer.replace(level=eval_buffer.level.at[t].set(new_eval))
-
-            train_nash, eval_nash = level_sampler.compute_nash(nash_rng, train_buffer, eval_buffer, t)
-
-            return (rng, train_buffer, eval_buffer, train_nash), None
-        
-        carry = (rng, train_buffer, eval_buffer, train_nash, eval_nash)
-        carry , _ = jax.lax.scan(_meta_do_loop, carry, jnp.arange(1, args.buffer_size), args.buffer_size - 1)
-
-        rng, train_buffer, eval_buffer, train_nash, eval_nash = carry
-
-        train_buffer = train_buffer.level.replace(
-            active = jnp.full_like(train_buffer.level, False)
-        )
-
-        # --- Initialize agent states ---
-        rng, _rng = jax.random.split(rng)
-        agent_states, _ = level_sampler.get_training_levels(_rng, train_buffer, train_nash, create_value_critic=False)
-        value_critic_states = None
-
-        lpg_train_step_fn = make_lpg_train_step(args, level_sampler)
-
-        def _meta_train_loop(carry, _):
-            rng, train_state, agent_states, value_critic_states, level_buffer = carry
-
-            # --- Update LPG ---
-            rng, _rng = jax.random.split(rng)
-            train_state, agent_states, value_critic_states, metrics = lpg_train_step_fn(
-                rng=_rng,
-                lpg_train_state=train_state,
-                agent_states=agent_states,
-                value_critic_states=value_critic_states,
-            )
-            carry = (rng, train_state, agent_states, value_critic_states, level_buffer)
-            return carry, metrics
-
-        carry = (rng, train_state, agent_states, value_critic_states, train_buffer)
-        carry, metrics = jax.lax.scan(
-            _meta_train_loop, carry, None, length=args.train_steps
-        )
-        rng, train_state, agent_states, _, level_buffer = carry
-            
-        return metrics, train_state, train_buffer
-
-    return _train_fn
-
 def make_train(args):
     def _train_fn(rng):
         # --- Initialize nash distributions ---
@@ -96,7 +24,7 @@ def make_train(args):
         train_state = create_lpg_train_state(train_rng, args)        
     
         # --- TRAIN LOOP ---
-        lpg_train_step_fn = make_lpg_train_step(args, level_sampler.rollout_manager)
+        lpg_train_step_fn = make_lpg_train_step(args, level_sampler)
         
         # --- Double Oracle (environment generating) Loop ---
         def _meta_train_loop(carry, t):
