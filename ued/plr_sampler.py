cedc7729d94ff1613cd9efb249713a215bdfc559
Nathan <nathanmonett1@gmail.com>
2024-07-28 10:51:45 +0100

    Added PLR swapping

--- ued/plr_sampler.py
+++ ued/plr_sampler.py
@@ -47,11 +47,13 @@ class PLRSampler(GDSampler):
         timestamp
     ):
         batch_size = len(train_levels.buffer_id)
+        rng, _rng = jax.random.split(rng)
+        level_buffer = self._reset_lowest_scoring(_rng, level_buffer, batch_size)
+
         rng, agent_rng, score_rng = jax.random.split(rng, 3)
         agent_rng = jax.random.split(agent_rng, batch_size)
-    
         eval_agents = jax.vmap(self._create_eval_agent, in_axes=(0, 0, None, None))(agent_rng, train_levels, actor_state, critic_state)
-        
+            
         score_rng = jax.random.split(rng, batch_size)
         scores = pmap(
             self._compute_algorithmic_regret, self.num_mini_batches
